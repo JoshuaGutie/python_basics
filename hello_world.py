113ea0d53c86a9d8972c367ed28cedee30902a46
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-10-15 07:15:21 -0500

    strings added to basics

--- hello_world.py
+++ hello_world.py
@@ -9,3 +9,8 @@ name = " ada lovelace"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+first_name = "ada"
+last_name = "smith"
+full_name = f'{first_name} {last_name}'
+print(full_name)
---
+++ name_cases.py
@@ -0,0 +1,14 @@
+name = "eric johnson"
+
+message = f"Hello, {name.title()}"
+print(message)
+
+print(name.upper())
+
+famous_person = "albert einstein"
+
+new_message = f"{famous_person} once said, 'a person who never made a mistake has never tried anyrhing new'"
+print(new_message)
+
+final_message = "l   Python"
+print(final_message.strip())
