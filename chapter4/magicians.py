19e465f2b3a07221073ca89c64f627b816a7a768
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-10-19 11:48:45 -0500

    loops

---
+++ chapter4/magicians.py
@@ -0,0 +1,4 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+#   print(magician)
+    print(f'{magician.title}, that was a great trick\n')
---
+++ chapter4/pizza.py
@@ -0,0 +1,7 @@
+pizzas = ['dominos', 'pizza hut', 'papa johns']
+for pizza in pizzas: 
+    print(f'I like {pizza.title()}')
+print('\nI love pizza!')
+
+for value in range(0, 8, 3):
+    print(value)
